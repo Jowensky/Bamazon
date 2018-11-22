1d3e5b01398b62dc3d6090157e4c12ec1e62b597
Jowensky Neard <jp.12@live.com>
2018-11-22 01:18:23 -0500

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,23 +14,40 @@ connection.connect(function(err) {
 
   var query = connection.query("SELECT * FROM bamazon", function(err, res) {
     for (var i = 0; i < res.length; i++) {
-      console.log(res[i]);
+      console.log(`ID: ${res[i]. ID} | Product: ${res[i].product_name} | Department: ${res[i].department_name} | Price: ${res[i].price} | Stock: ${res[i].stock_quantity}`);
     }
     inquirer
       .prompt([
         {
           name: "id",
           message: "What is the (ID) of the item you would like to purchase?"
-        },
+        }, 
         {
-          name: "amount",
-          message: "What is the quanity of you purchase?"
+        name: "quanity",
+        message: "What is the quanity you want to purchase"
         }
       ])
       .then(function(purchase) {
-        var id = purchase.id
-        var amount = purchase.amount
+          var itemId = purchase.id
+          var Cquanity = purchase.quanity
+         
+          var query = connection.query(`SELECT * FROM bamazon WHERE ID = ${itemId}`, function(err, res) {
+                stock = res[0].stock_quantity
+                price = res[0].price
+            if (Cquanity > stock) {
+                console.log("Insufficient quantity!")
+                connection.end();
+            } else {
+                stockLeft = stock - Cquanity
+                cost = price * Cquanity 
+                var query = connection.query(`UPDATE bamazon SET stock_quantity = ${stockLeft} WHERE ID = ${itemId} `, function (err, res) {
+                    console.log(`Your purchase have been made! 
+                    Your total is: $${cost}`)
+                    connection.end();
+                })
+            }
+          })
+          })
       });
   });
-  connection.end();
-});
+
